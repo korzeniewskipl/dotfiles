3dee4e3d21f0b9fa55346c61dc7e5458ebefcaa0
Paweł Korzeniewski <pawel@korzeniewski.pl>
2017-08-16 23:21:21 +0200

    Remove .bashrc and .Xmodmap files.

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-wget -qO ~/.Xmodmap https://raw.githubusercontent.com/korzeniewskipl/dotfiles/master/.Xmodmap
 wget -qO ~/.bash_aliases https://raw.githubusercontent.com/korzeniewskipl/dotfiles/master/.bash_aliases
-wget -qO ~/.bashrc https://raw.githubusercontent.com/korzeniewskipl/dotfiles/master/.bashrc
 wget -qO ~/.gitconfig https://raw.githubusercontent.com/korzeniewskipl/dotfiles/master/.gitconfig
 wget -qO ~/.tmux.conf https://raw.githubusercontent.com/korzeniewskipl/dotfiles/master/.tmux.conf
